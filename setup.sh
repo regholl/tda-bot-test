88ef4d9f44d77c41ba111f55ec7e3b83f1598f61
Tyler Potts <twpotts11@gmail.com>
2022-08-25 19:41:52 -0500

    no quotes

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 [theme]\n\
-primaryColor = "#E694FF"\n\
-backgroundColor = "#00172B"\n\
-secondaryBackgroundColor = "#0083B8"\n\
-textColor = "#FFF"\n\
-font = "sans serif"\n\
+primaryColor = #E694FF\n\
+backgroundColor = #00172B\n\
+secondaryBackgroundColor = #0083B8\n\
+textColor = #FFF\n\
+font = sans serif\n\
 " > ~/.streamlit/config.toml
